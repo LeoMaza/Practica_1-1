4e5d1fa217999e207bf3fdb722bdcd728134997e
Leo Maza <leomaza2222@gmail.com>
2022-04-05 12:34:33 -0300

    aguregue archivo .js

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+document.write("<h1>Mi Primera Pagina Web</h1>");
+document.write("<img src='https://st.depositphotos.com/1448094/5111/v/600/depositphotos_51110295-stock-illustration-space-landscape.jpg'>"
+);
+
+
+
